3c19c3acf7f723d8dabdaafbe23a5184a0d9b4cc
kumiko-itashiki <k-itashiki@tryt-group.co.jp>
2023-12-17 13:37:45 +0900

    課題再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,7 +1,7 @@
 //変数の宣言・値の代入
 let bodyWeight = 68;
 let bodyHeight = 1.7;
-let BMI = bodyWeight / [bodyHeight * bodyHeight];
+let BMI = bodyWeight / (bodyHeight * bodyHeight);
 
 //コンソールへの出力
 console.log(BMI);
