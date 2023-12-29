efb818eddd1dcf72d84d8a6b1a0a33e8658c98f7
kumiko-itashiki <k-itashiki@tryt-group.co.jp>
2023-12-29 15:42:20 +0900

    課題提出

---
+++ kadai_014/square.js
@@ -0,0 +1,12 @@
+//与えられた引数numを２乗にし、その値を戻り値をして返す関すを定義する：アロー関数
+const square = (num) => {
+  return num ** 2;
+};
+
+//与えられた引数numを２乗にし、その値を戻り値をして返す関すを定義する：関数宣言
+// function square(num) {
+//   return num ** 2;
+// }
+
+//関数の戻り値を出力する
+console.log(square(30));
